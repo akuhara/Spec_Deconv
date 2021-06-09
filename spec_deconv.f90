f178e93e86295cc54889f5ef3b2c13b9d300d274
Takeshi Akuhara <akuhara@eri.u-tokyo.ac.jp>
2021-06-09 11:14:47 +0900

    add license

--- spec_deconv.f90
+++ spec_deconv.f90
@@ -1,6 +1,31 @@
-!-----------------------------------------------------------------------
-! Ad-hoc wiener filter method to calculate RFs
-!-----------------------------------------------------------------------
+!=======================================================================
+!   Spec_Deconv 
+!   Receiver function estimation by spectral division with 
+!   frequency-dependent damping
+!   Copyright (C) 2021 Takeshi Akuhara
+!
+!   This program is free software: you can redistribute it and/or modify
+!   it under the terms of the GNU General Public License as published by
+!   the Free Software Foundation, either version 3 of the License, or
+!   (at your option) any later version.
+!
+!   This program is distributed in the hope that it will be useful,
+!   but WITHOUT ANY WARRANTY; without even the implied warranty of
+!   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!   GNU General Public License for more details.
+!
+!   You should have received a copy of the GNU General Public License
+!   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!
+!
+!   Contact information
+!
+!   Email  : akuhara @ eri. u-tokyo. ac. jp 
+!   Address: Earthquake Research Institute, The Univesity of Tokyo   
+!           1-1-1, Yayoi, Bunkyo-ku, Tokyo 113-0032, Japan
+!
+!=======================================================================
+
 module const
   implicit none 
   integer, parameter :: ncmp = 3, iz = 1, ir = 2, it = 3
